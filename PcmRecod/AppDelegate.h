e2918c2ab5d079fcb482955545b835397f77b6fe
mihuaz2Z <mihuayouxi@163.com>
2017-05-31 10:48:19 +0800

    Initial Commit

---
+++ PcmRecod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PcmRecod
+//
+//  Created by Ruiwen Feng on 2017/5/31.
+//  Copyright © 2017年 Ruiwen Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PcmRecod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PcmRecod
+//
+//  Created by Ruiwen Feng on 2017/5/31.
+//  Copyright © 2017年 Ruiwen Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
